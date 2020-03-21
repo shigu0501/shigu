752b62d9737315597af2e4cf51cc138d2ebf1e43
井原志五 <shigu0501@gmail.com>
2020-03-21 12:21:48 +0000

    エラー修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,11 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(text: params[:text], tweet_id: comment_params[:tweet_id], current_user.id)
+    Comment.create(text: params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
   end
 
-private
-def comment_params
-  params.permit(:text, :tweet_id)
-end
+  private
+
+  def comment_params
+    params.permit(:text, :tweet_id)
+  end
 end
